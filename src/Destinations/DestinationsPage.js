053cf41a6dec22ae02bb1a452ab366be1686b030
amlight1938 <102883052+amlight1938@users.noreply.github.com>
2023-03-07 23:46:30 -0700

    Update DestinationsPage.js

--- src/Destinations/DestinationsPage.js
+++ src/Destinations/DestinationsPage.js
@@ -3,7 +3,7 @@ import campfire_field from '../Assets/campfire_field.jpg'
 import TopBanner from "../TopBanner";
 
 const DestinationsPage = () => {
-    const api_url = 'https://deploy-branch.d2t5sfhoijxb3g.amplifyapp.com/api/v1/trips'
+    const api_url = 'https://test-branch.d2wtxxea61f4fu.amplifyapp.com//api/v1/trips'
 
     return (
         <>
